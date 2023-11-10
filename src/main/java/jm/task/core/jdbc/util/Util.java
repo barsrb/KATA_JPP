3d7eac24423b04197155e34a62a6f38ab58156dc
Mikhail Pershukov <dev@76543210.ru>
2023-11-10 17:51:56 +0300

    Use Postgresql. Add util method to get connection. Add methods to create and delete table.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5432/kpp_1_2_3", "kpp_1_2_3", "kpp_1_2_3");
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
