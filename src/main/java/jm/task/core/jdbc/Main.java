ff12cd426cf98480c6a005656b7d76e2d49b8099
Mikhail Pershukov <dev@76543210.ru>
2023-11-14 09:14:53 +0300

    Add connector to hibernate.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,9 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -11,26 +14,26 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
-        User u1 = new User("u1_name", "u1_Lastame", (byte) 1);
-        userService.saveUser(u1.getName(), u1.getLastName(), u1.getAge());
-        System.out.println("User с именем – " + u1.getName() + " добавлен в базу данных");
-
-        User u2 = new User("u2_name", "u2_Lastame", (byte) 2);
-        userService.saveUser(u2.getName(), u2.getLastName(), u2.getAge());
-        System.out.println("User с именем – " + u2.getName() + " добавлен в базу данных");
-
-        User u3 = new User("u3_name", "u3_Lastame", (byte) 3);
-        userService.saveUser(u3.getName(), u3.getLastName(), u3.getAge());
-        System.out.println("User с именем – " + u3.getName() + " добавлен в базу данных");
-
-        User u4 = new User("u4_name", "u4_Lastame", (byte) 4);
-        userService.saveUser(u4.getName(), u4.getLastName(), u4.getAge());
-        System.out.println("User с именем – " + u4.getName() + " добавлен в базу данных");
-
-        List<User> users = userService.getAllUsers();
-        System.out.println(users);
-
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        User u1 = new User("u1_name", "u1_Lastame", (byte) 1);
+//        userService.saveUser(u1.getName(), u1.getLastName(), u1.getAge());
+//        System.out.println("User с именем – " + u1.getName() + " добавлен в базу данных");
+//
+//        User u2 = new User("u2_name", "u2_Lastame", (byte) 2);
+//        userService.saveUser(u2.getName(), u2.getLastName(), u2.getAge());
+//        System.out.println("User с именем – " + u2.getName() + " добавлен в базу данных");
+//
+//        User u3 = new User("u3_name", "u3_Lastame", (byte) 3);
+//        userService.saveUser(u3.getName(), u3.getLastName(), u3.getAge());
+//        System.out.println("User с именем – " + u3.getName() + " добавлен в базу данных");
+//
+//        User u4 = new User("u4_name", "u4_Lastame", (byte) 4);
+//        userService.saveUser(u4.getName(), u4.getLastName(), u4.getAge());
+//        System.out.println("User с именем – " + u4.getName() + " добавлен в базу данных");
+//
+//        List<User> users = userService.getAllUsers();
+//        System.out.println(users);
+//
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static Connection getConnection() {
@@ -16,5 +24,38 @@ public class Util {
             e.printStackTrace();
         }
         return conn;
+    }
+
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.URL, "jdbc:postgresql://127.0.0.1:5432/kpp_1_2_3");
+                settings.put(Environment.USER, "kpp_1_2_3");
+                settings.put(Environment.PASS, "kpp_1_2_3");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
